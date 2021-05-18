c0a785fec9035187fae39ec514c9ca5d3381a040
tomhobson <tomhobsonleeds@gmail.com>
2021-05-18 13:45:07 +0100

    update azurerm version

--- cluster/terraform-jx-azurekeyvault/main.tf
+++ cluster/terraform-jx-azurekeyvault/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_version = ">= 0.13.2"
   required_providers {
     azurerm = {
-      version = ">=2.41.0"
+      version = ">=2.56.0"
     }
   }
 }
--- cluster/terraform-jx-registry-acr/main.tf
+++ cluster/terraform-jx-registry-acr/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_version = ">= 0.13.2"
   required_providers {
     azurerm = {
-      version = ">=2.39.0"
+      version = ">=2.56.0"
     }
   }
 }
