1aa7c2e2b23aa6c000fe9b26bbbfdf656aa5328a
tomhobson <tomhobsonleeds@gmail.com>
2021-06-10 16:01:27 +0100

    fix resource group name

--- cluster/terraform-jx-azuredns/outputs.tf
+++ cluster/terraform-jx-azuredns/outputs.tf
@@ -5,7 +5,7 @@ output "name_servers" {
   value = length(azurerm_dns_zone.dns) == 0 ? [] : azurerm_dns_zone.dns[0].name_servers
 }
 output "resource_group_name" {
-  value = length(azurerm_dns_zone.dns) == 0 ? data.azurerm_dns_zone.apex_domain_zone.0.name : azurerm_dns_zone.dns[0].name
+  value = length(azurerm_dns_zone.dns) == 0 ? data.azurerm_dns_zone.apex_domain_zone.0.resource_group_name : azurerm_resource_group.dns[0].name
 }
 output "subscription_id" {
   value = data.azurerm_subscription.current.subscription_id
