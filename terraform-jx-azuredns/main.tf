e60349f6ed6e22fd714d21ad0f89fb60d07bfc6a
Poki <peter.pokoraczki@gmail.com>
2021-06-18 14:30:55 +0200

    fix: add ability to use root apex domain

--- terraform-jx-azuredns/main.tf
+++ terraform-jx-azuredns/main.tf
@@ -7,28 +7,32 @@ terraform {
   }
 }
 
-data "azurerm_dns_zone" "apex_domain_zone" {
-  count = var.apex_domain != "" ? 1 : 0
-  name  = var.apex_domain
-}
-
 data "azurerm_subscription" "current" {
 }
 
+data "azurerm_dns_zone" "apex_domain_zone" {
+  count = local.enabled ? 1 : 0
+  name  = var.apex_domain
+}
+
+data "azurerm_resource_group" "apex_resource_group" {
+  name = var.apex_resource_group_name
+}
+
 resource "azurerm_resource_group" "dns" {
-  count    = 1
+  count    = local.enabled && local.with_subdomain ? 1 : 0
   name     = local.resource_group_name
   location = var.location
 }
 
 resource "azurerm_dns_zone" "dns" {
-  count               = var.apex_domain != "" && var.subdomain != "" ? 1 : 0
+  count               = local.enabled && local.with_subdomain ? 1 : 0
   name                = join(".", [var.subdomain, var.apex_domain])
   resource_group_name = azurerm_resource_group.dns.0.name
 }
 
 resource "azurerm_dns_ns_record" "subdomain_ns_delegation" {
-  count               = var.apex_domain != "" && var.subdomain != "" && var.apex_domain_integration_enabled ? 1 : 0
+  count               = local.enabled && local.with_subdomain && var.apex_domain_integration_enabled ? 1 : 0
   name                = var.subdomain
   zone_name           = data.azurerm_dns_zone.apex_domain_zone.0.name
   resource_group_name = var.apex_resource_group_name
@@ -38,8 +42,16 @@ resource "azurerm_dns_ns_record" "subdomain_ns_delegation" {
 }
 
 resource "azurerm_role_assignment" "Give_ExternalDNS_SP_Contributor_Access_to_ResourceGroup" {
-  count                = 1
+  count                = local.enabled && local.with_subdomain && var.apex_domain_integration_enabled ? 1 : 0
   scope                = azurerm_resource_group.dns.0.id
   role_definition_name = "DNS Zone Contributor"
   principal_id         = var.principal_id
 }
+
+resource "azurerm_role_assignment" "Give_ExternalDNS_SP_Contributor_Access_to_ResourceGroup_to_Apex" {
+  count                = !local.with_subdomain && var.apex_domain_integration_enabled ? 1 : 0
+  scope                = data.azurerm_resource_group.apex_resource_group.id
+  role_definition_name = "DNS Zone Contributor"
+  principal_id         = var.principal_id
+}
+
