51012a356616690c1fa180f25e9e49a6ac95fcd8
tomhobson <tomhobsonleeds@gmail.com>
2021-06-18 13:35:25 +0100

    update tf output to use proper cr name

--- cluster/terraform-jx-registry-acr/outputs.tf
+++ cluster/terraform-jx-registry-acr/outputs.tf
@@ -1,5 +1,5 @@
 output "registry_name" {
-  value = var.external_registry_url != "" ? var.external_registry_url : var.use_existing_acr_name == null ? azurerm_container_registry.acr[0].name : data.azurerm_container_registry.acr_existing[0].name
+  value = var.external_registry_url != "" ? var.external_registry_url : var.use_existing_acr_name == null ? lower("${azurerm_container_registry.acr[0].name }.azurecr.io") : data.azurerm_container_registry.acr_existing[0].name
 }
 output "admin_username" {
   value = var.external_registry_url != "" ? "" : var.use_existing_acr_name == null ? azurerm_container_registry.acr[0].admin_username : data.azurerm_container_registry.acr_existing[0].admin_username
