1b2dd32ada62e37dfb6fb9320a14708fa34fa6f8
tomhobson <tomhobsonleeds@gmail.com>
2022-01-01 18:33:07 +0000

    feat: readd azure spot
    This reverts commit 320635450d01c2ee7e3a9cf0e3f02912190df0ec.

--- variables.tf
+++ variables.tf
@@ -30,6 +30,10 @@ variable "jx_bot_token" {
 // ----------------------------------------------------------------------------
 // Machine variables
 // ----------------------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// System nodepool variables
+// ----------------------------------------------------------------------------
 variable "node_size" {
   type        = string
   default     = "Standard_B2ms"
@@ -50,10 +54,13 @@ variable "max_node_count" {
   type        = number
   default     = null
 }
+// ----------------------------------------------------------------------------
+// Machine learning nodepool variables
+// ----------------------------------------------------------------------------
 variable "ml_node_size" {
   type        = string
   default     = ""
-  description = "The size of the ML node to use for the cluster"
+  description = "The size of the worker node to use for the cluster"
 }
 variable "ml_node_count" {
   description = "The number of ML nodes to use for the cluster"
@@ -67,6 +74,40 @@ variable "min_ml_node_count" {
 }
 variable "max_ml_node_count" {
   description = "The maximum number of ML nodes to use for the cluster if autoscaling is enabled"
+  type        = number
+  default     = null
+}
+
+// ----------------------------------------------------------------------------
+// Build nodepool variables
+// ----------------------------------------------------------------------------
+variable "use_spot" {
+  type        = bool
+  default     = true
+  description = "Should we use spot instances for the build nodes"
+}
+variable "spot_max_price" {
+  type        = number
+  default     = -1
+  description = "The maximum price you're willing to pay in USD per virtual machine, -1 to go to the maximum price"
+}
+variable "build_node_size" {
+  type        = string
+  default     = ""
+  description = "The size of the build node to use for the cluster"
+}
+variable "build_node_count" {
+  description = "The number of build nodes to use for the cluster"
+  type        = number
+  default     = null
+}
+variable "min_build_node_count" {
+  description = "The minimum number of builder nodes to use for the cluster if autoscaling is enabled"
+  type        = number
+  default     = null
+}
+variable "max_build_node_count" {
+  description = "The maximum number of builder nodes to use for the cluster if autoscaling is enabled"
   type        = number
   default     = null
 }
