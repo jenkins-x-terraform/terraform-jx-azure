9c09d559446617b042ced7eda11fa9fc769ba8ac
mentlak0 <mentlak@outlook.com>
2022-09-08 11:16:54 +0100

    feat: created new node pool and changes to ml node spot

--- cluster/terraform-jx-cluster-aks/cluster/main.tf
+++ cluster/terraform-jx-cluster-aks/cluster/main.tf
@@ -63,6 +63,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "mlnode" {
   count                 = var.ml_node_size == "" ? 0 : 1
   name                  = "mlnode"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  priority              = var.use_spot ? "Spot" : "Regular"
   vm_size               = var.ml_node_size
   vnet_subnet_id        = var.vnet_subnet_id
   node_count            = var.ml_node_count
@@ -76,6 +77,23 @@ resource "azurerm_kubernetes_cluster_node_pool" "mlnode" {
 resource "azurerm_kubernetes_cluster_node_pool" "buildnode" {
   count                 = var.build_node_size == "" ? 0 : 1
   name                  = "buildnode"
+  priority              = var.use_spot ? "Spot" : "Regular"
+  eviction_policy       = var.use_spot ? "Deallocate" : null
+  spot_max_price        = var.use_spot ? var.spot_max_price : null
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = var.build_node_size
+  vnet_subnet_id        = var.vnet_subnet_id
+  node_count            = var.use_spot ? 0 : var.build_node_count
+  min_count             = var.min_build_node_count
+  max_count             = var.max_build_node_count
+  orchestrator_version  = var.cluster_version
+  enable_auto_scaling   = var.max_build_node_count == null ? false : true
+  node_taints = ["sku=build:NoSchedule"]
+}
+
+resource "azurerm_kubernetes_cluster_node_pool" "infranode" {
+  count                 = var.build_node_size == "" ? 0 : 1
+  name                  = "infranode"
   priority              = var.use_spot ? "Spot" : "Regular"
   eviction_policy       = var.use_spot ? "Deallocate" : null
   spot_max_price        = var.use_spot ? var.spot_max_price : null
