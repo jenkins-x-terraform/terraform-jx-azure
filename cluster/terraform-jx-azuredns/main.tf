dd6cf29c60ca224e10b5bb98b0a140f7b645afb6
mentlak0 <mentlak@outlook.com>
2022-11-09 15:57:03 +0000

    Revert "feat: add version upgrade TF 1.3.2"
    This reverts commit 8df1c92fc61f361f6501e3d6973bf3dbda128de3.

--- cluster/terraform-jx-azuredns/main.tf
+++ cluster/terraform-jx-azuredns/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.2"
+  required_version = ">= 0.13.2"
   required_providers {
     azurerm = {
       version = ">=2.39.0"
--- cluster/terraform-jx-boot/main.tf
+++ cluster/terraform-jx-boot/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.2"
+  required_version = ">= 0.13.2"
   required_providers {
     helm = {
       version = ">=2.0.1"
