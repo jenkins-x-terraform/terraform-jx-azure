00d9628022e1b1e072b94a30b974a2a5359bb9e2
Skisocks <jaa.skeoch@gmail.com>
2023-02-02 14:58:30 +0000

    fix: apparently indexing isn't needed everywhere?

--- cluster/terraform-jx-registry-acr/main.tf
+++ cluster/terraform-jx-registry-acr/main.tf
@@ -36,13 +36,13 @@ resource "azurerm_container_registry" "oss_acr" {
 }
 
 resource "azurerm_role_assignment" "oss_pull" {
-  scope                = azurerm_container_registry.oss_acr[0].id
+  scope                = azurerm_container_registry.oss_acr.id
   role_definition_name = "AcrPull"
   principal_id = "00000000-0000-0000-0000-000000000000"
 }
 
 resource "azurerm_role_assignment" "oss_push" {
-  scope                = azurerm_container_registry.oss_acr[0].id
+  scope                = azurerm_container_registry.oss_acr.id
   role_definition_name = "AcrPush"
   principal_id         = var.principal_id
 }
