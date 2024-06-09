5cdfcf1d610a8ad2f2b866bc46e515fe87761a04
mentlak0 <mentlak@outlook.com>
2024-06-09 17:53:40 +0100

    chore: fix outputs when empty

--- cluster/terraform-jx-registry-acr/outputs.tf
+++ cluster/terraform-jx-registry-acr/outputs.tf
@@ -1,12 +1,23 @@
 output "registry_name" {
-  value = var.external_registry_url != "" ? var.external_registry_url : var.use_existing_acr_name == null ? lower("${azurerm_container_registry.acr[0].name }.azurecr.io") : data.azurerm_container_registry.acr_existing[0].name
+  value = var.external_registry_url != "" ? var.external_registry_url :
+  (var.use_existing_acr_name == null && length(azurerm_container_registry.acr) > 0) ? lower("${azurerm_container_registry.acr[0].name}.azurecr.io") :
+  data.azurerm_container_registry.acr_existing[0].name
 }
+
 output "admin_username" {
-  value = var.external_registry_url != "" ? "" : var.use_existing_acr_name == null ? azurerm_container_registry.acr[0].admin_username : data.azurerm_container_registry.acr_existing[0].admin_username
+  value = var.external_registry_url != "" ? "" :
+  (var.use_existing_acr_name == null && length(azurerm_container_registry.acr) > 0) ? azurerm_container_registry.acr[0].admin_username :
+  data.azurerm_container_registry.acr_existing[0].admin_username
 }
+
 output "admin_password" {
-  value = var.external_registry_url != "" ? "" : var.use_existing_acr_name == null ? azurerm_container_registry.acr[0].admin_password : data.azurerm_container_registry.acr_existing[0].admin_password
+  value = var.external_registry_url != "" ? "" :
+  (var.use_existing_acr_name == null && length(azurerm_container_registry.acr) > 0) ? azurerm_container_registry.acr[0].admin_password :
+  data.azurerm_container_registry.acr_existing[0].admin_password
 }
+
 output "resource_group_name" {
-  value = var.external_registry_url != "" ? "" : var.use_existing_acr_resource_group_name == null ? azurerm_container_registry.acr[0].resource_group_name : data.azurerm_container_registry.acr_existing[0].resource_group_name
+  value = var.external_registry_url != "" ? "" :
+  (var.use_existing_acr_resource_group_name == null && length(azurerm_container_registry.acr) > 0) ? azurerm_container_registry.acr[0].resource_group_name :
+  data.azurerm_container_registry.acr_existing[0].resource_group_name
 }
