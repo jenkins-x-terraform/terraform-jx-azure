574e42f24a25f4f929d725cef1babcff57dc3011
mentlak0 <mentlak@outlook.com>
2024-06-13 11:28:30 +0100

    feat: add kublet id as outputs

--- output.tf
+++ output.tf
@@ -5,3 +5,10 @@ output "registry_name" {
 output "resource_group_name" {
   value = module.cluster.resource_group_name
 }
+
+output "kubelet_identity_id" {
+  value = module.cluster.kubelet_identity_id
+}
+output "kubelet_client_id" {
+  value = module.cluster.kubelet_client_id
+}
