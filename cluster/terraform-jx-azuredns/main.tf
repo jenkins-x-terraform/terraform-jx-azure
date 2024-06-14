262787c2692377e191b649f8d03bedc19adcd4de
mentlak0 <mentlak@outlook.com>
2024-06-14 12:10:23 +0100

    fix: stupid terraform

--- cluster/terraform-jx-azuredns/main.tf
+++ cluster/terraform-jx-azuredns/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_dns_zone" "dns" {
 resource "azurerm_dns_ns_record" "subdomain_ns_delegation" {
   count               = local.enabled && ((local.with_subdomain && var.apex_domain_integration_enabled) || var.dns_resources_enabled) ? 1 : 0
   name                = var.subdomain
-  zone_name           = data.azurerm_dns_zone.apex_domain_zone.0.name
+  zone_name           = length(azurerm_dns_zone.dns) > 0 ? azurerm_dns_zone.dns[0].name_servers : []
   resource_group_name = var.apex_resource_group_name
   ttl                 = 60
   records             = azurerm_dns_zone.dns[0].name_servers
@@ -44,14 +44,14 @@ resource "azurerm_dns_ns_record" "subdomain_ns_delegation" {
 
 resource "azurerm_role_assignment" "Give_ExternalDNS_SP_Contributor_Access_to_ResourceGroup" {
   count                = local.enabled && ((local.with_subdomain && var.apex_domain_integration_enabled) || var.dns_resources_enabled) ? 1 : 0
-  scope                = azurerm_resource_group.dns.id
+  scope                = azurerm_resource_group.dns.0.id
   role_definition_name = "DNS Zone Contributor"
   principal_id         = var.principal_id
 }
 
 resource "azurerm_role_assignment" "Give_ExternalDNS_SP_Contributor_Access_to_ResourceGroup_to_Apex" {
   count                = !local.with_subdomain && (var.apex_domain_integration_enabled || var.dns_resources_enabled) ? 1 : 0
-  scope                = data.azurerm_resource_group.apex_resource_group.id
+  scope                = length(data.azurerm_resource_group.apex_resource_group) > 0 ? data.azurerm_resource_group.apex_resource_group[0].id : ""
   role_definition_name = "DNS Zone Contributor"
   principal_id         = var.principal_id
 }
