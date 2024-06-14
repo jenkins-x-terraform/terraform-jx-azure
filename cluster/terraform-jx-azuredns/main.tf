dbdca78e3d44643c9c0b109d3bee65ba8dc6dc73
mentlak0 <mentlak@outlook.com>
2024-06-14 12:17:50 +0100

    fix: stupid terraform

--- cluster/terraform-jx-azuredns/main.tf
+++ cluster/terraform-jx-azuredns/main.tf
@@ -35,10 +35,10 @@ resource "azurerm_dns_zone" "dns" {
 resource "azurerm_dns_ns_record" "subdomain_ns_delegation" {
   count               = local.enabled && ((local.with_subdomain && var.apex_domain_integration_enabled) || var.dns_resources_enabled) ? 1 : 0
   name                = var.subdomain
-  zone_name           = length(azurerm_dns_zone.dns) > 0 ? azurerm_dns_zone.dns[0].name_servers : []
+  zone_name           = length(data.azurerm_dns_zone.apex_domain_zone) > 0 ? data.azurerm_dns_zone.apex_domain_zone[0].name : ""
   resource_group_name = var.apex_resource_group_name
   ttl                 = 60
-  records             = azurerm_dns_zone.dns[0].name_servers
+  records             = length(azurerm_dns_zone.dns) > 0 ? azurerm_dns_zone.dns[0].name_servers : []
   depends_on          = [azurerm_dns_zone.dns]
 }
 
