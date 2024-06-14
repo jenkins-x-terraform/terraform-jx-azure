09ba28705bbd147860be40b7e2c323081f049733
mentlak0 <mentlak@outlook.com>
2024-06-14 21:44:13 +0100

    chore: add output

--- cluster/terraform-jx-registry-acr/outputs.tf
+++ cluster/terraform-jx-registry-acr/outputs.tf
@@ -2,6 +2,10 @@ output "registry_name" {
   value = var.external_registry_url != "" ? var.external_registry_url : (var.use_existing_acr_name == null && length(azurerm_container_registry.acr) > 0 ? lower("${azurerm_container_registry.acr[0].name}.azurecr.io") : (length(data.azurerm_container_registry.acr_existing) > 0 ? data.azurerm_container_registry.acr_existing[0].name : ""))
 }
 
+output "registry_name_acr" {
+  value = var.external_registry_url != "" ? var.external_registry_url : azurerm_container_registry.acr.name
+}
+
 output "admin_username" {
   value = var.external_registry_url != "" ? "" : (var.use_existing_acr_name == null && length(azurerm_container_registry.acr) > 0 ? azurerm_container_registry.acr[0].admin_username : (length(data.azurerm_container_registry.acr_existing) > 0 ? data.azurerm_container_registry.acr_existing[0].admin_username : ""))
 }
