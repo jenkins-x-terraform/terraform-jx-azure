cf1f07286dda47d7347e72685cf23f90779a5b4d
mentlak0 <mentlak@outlook.com>
2024-06-14 14:57:22 +0100

    chore: fix rsg

--- cluster/terraform-jx-azuredns/outputs.tf
+++ cluster/terraform-jx-azuredns/outputs.tf
@@ -5,7 +5,7 @@ output "name_servers" {
   value = local.enabled && local.with_subdomain ? join(",", azurerm_dns_zone.dns.0.name_servers) : "--"
 }
 output "resource_group_name" {
-  value = local.enabled && local.with_subdomain ? azurerm_resource_group.dns.0.name : var.apex_resource_group_name
+  value = local.enabled && local.with_subdomain ? azurerm_resource_group.dns.0.name : var.resource_group_name
 }
 output "subscription_id" {
   value = data.azurerm_subscription.current.subscription_id
