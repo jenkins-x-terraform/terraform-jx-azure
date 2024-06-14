7cf47faeda6bb1ffa70d9af57d3a46f4d6a9cb5e
mentlak0 <mentlak@outlook.com>
2024-06-14 12:05:19 +0100

    fix: stupid terraform

--- cluster/terraform-jx-azuredns/main.tf
+++ cluster/terraform-jx-azuredns/main.tf
@@ -44,14 +44,14 @@ resource "azurerm_dns_ns_record" "subdomain_ns_delegation" {
 
 resource "azurerm_role_assignment" "Give_ExternalDNS_SP_Contributor_Access_to_ResourceGroup" {
   count                = local.enabled && ((local.with_subdomain && var.apex_domain_integration_enabled) || var.dns_resources_enabled) ? 1 : 0
-  scope                = azurerm_resource_group.dns.0.id
+  scope                = azurerm_resource_group.dns.id
   role_definition_name = "DNS Zone Contributor"
   principal_id         = var.principal_id
 }
 
 resource "azurerm_role_assignment" "Give_ExternalDNS_SP_Contributor_Access_to_ResourceGroup_to_Apex" {
   count                = !local.with_subdomain && (var.apex_domain_integration_enabled || var.dns_resources_enabled) ? 1 : 0
-  scope                = data.azurerm_resource_group.apex_resource_group.0.id
+  scope                = data.azurerm_resource_group.apex_resource_group.id
   role_definition_name = "DNS Zone Contributor"
   principal_id         = var.principal_id
 }
