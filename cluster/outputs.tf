145a00f369e2d09933fe7b8fe69bed932070e241
mentlak0 <mentlak@outlook.com>
2024-06-16 15:50:50 +0100

    chore: amend outputs

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -9,7 +9,3 @@ output "resource_group_name" {
 output "kubelet_identity_id" {
   value = module.cluster.kubelet_identity_id
 }
-
-output "kubelet_client_id" {
-  value = module.cluster.kubelet_client_id
-}
